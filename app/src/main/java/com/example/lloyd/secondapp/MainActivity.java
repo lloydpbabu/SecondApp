907b7ef8745d6a101f12131fc52ad3d68bfff67e
lloydpbabu <lloydpbabu@gmail.com>
2017-02-23 18:49:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lloyd/secondapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lloyd.secondapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button Btn1;
+
+    public void init()
+    {
+     Btn1= (Button) findViewById(R.id.Btn1);
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSecondActivity= new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(toSecondActivity);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+    }
+}
